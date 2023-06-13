69dcbc2f89bd6de84726cfa5075f32c42b3e7216
Shaunak Jagtap <shaunakjagtap@gmail.com>
2023-06-14 02:06:42 +0530

    fix: updated socket io in spm

--- Package.swift
+++ Package.swift
@@ -12,16 +12,15 @@ let package = Package(
             targets: ["DyteiOSCore"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ethand91/mediasoup-ios-client.git", from: "1.5.4"),
-        .package(url: "https://github.com/dyte-in/DyteiOSSocketIO.git", from: "0.0.1"),
+        .package(url: "https://github.com/dyte-in/DyteiOSSocketIO.git", from: "0.0.3"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
                     name: "DyteiOSCore",
-                    url: "https://github.com/dyte-in/DyteMobileCoreiOS/archive/refs/tags/0.1.4.zip",
-                    checksum: "dd779c0ca84e41f6f416ff9f722d005b8f9c255490ef8f939c70dff87bf954a2"
+                    url: "https://github.com/dyte-in/DyteMobileCoreiOS/archive/refs/tags/0.4.2.zip",
+                    checksum: "308b3aa45088edef24aee1c5e62d6a9dd86dbdd437ce2821711a8e345ca8a176"
                 )
     ]
 )
