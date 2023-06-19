122f09b13140d03c3fe7719230e61e605e93e176
Shaunak Jagtap <shaunakjagtap@gmail.com>
2023-06-19 19:42:24 +0530

    Updated checksum

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         .binaryTarget(
                     name: "DyteiOSCore",
                     url: "https://github.com/dyte-in/DyteMobileCoreiOS/archive/refs/tags/0.4.5.zip",
-                    checksum: "308b3aa45088edef24aee1c5e62d6a9dd86dbdd437ce2821711a8e345ca8a176"
+                    checksum: "379b6f36d898032949208e19ffdc31ad50982d12b6049d10f58b3243770d6d17"
                 )
     ]
 )
