527ab0f8b45b94315d76e9a32e0885b204795b5a
Shaunak Jagtap <shaunakjagtap@gmail.com>
2023-06-21 14:54:42 +0530

    release: 0.4.6
    postal crash fix

--- Package.swift
+++ Package.swift
@@ -12,14 +12,16 @@ let package = Package(
             targets: ["DyteiOSCore"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/dyte-in/DyteiOSSocketIO.git", from: "0.0.3"),
+//        .package(url: "https://github.com/dyte-in/DyteiOSSocketIO.git", from: "0.0.3"),
+//        .package(url: "https://github.com/jitsi/webrtc.git", from: "111.0.2"),
+//        .package(url: "https://github.com/daltoniam/Starscream", from: "4.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
                     name: "DyteiOSCore",
-                    url: "https://github.com/dyte-in/DyteMobileCoreiOS/archive/refs/tags/0.4.5.zip",
+                    url: "https://github.com/dyte-in/DyteMobileCoreiOS/archive/refs/tags/0.4.6.zip",
                     checksum: "379b6f36d898032949208e19ffdc31ad50982d12b6049d10f58b3243770d6d17"
                 )
     ]
