3a745cce2391c892346e862d1895903186223fa8
Shaunak Jagtap <shaunakjagtap@gmail.com>
2023-06-23 21:26:03 +0530

    fix: spm fix

--- Package.swift
+++ Package.swift
@@ -1,27 +1,30 @@
-// swift-tools-version: 5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
-    name: "DyteMobileCoreiOS",
+    name: "DyteiOSCore",
+    platforms: [.iOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "DyteiOSCore",
-            targets: ["DyteiOSCore"]),
+        .library(name: "DyteiOSCore", targets: ["DyteiOSCoreTargets"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/dyte-in/DyteiOSSocketIO.git", from: "0.0.4"),
-        .package(url: "https://github.com/jitsi/webrtc.git", from: "111.0.2"),
+        .package(url: "https://github.com/dyte-in/DyteiOSSocketIO.git", branch: "main"),
+        .package(name: "WebRTC", url: "https://github.com/jitsi/webrtc.git", branch: "master"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
-                    name: "DyteiOSCore",
-                    url: "https://github.com/dyte-in/DyteMobileCoreiOS/archive/refs/tags/0.4.7.zip",
-                    checksum: "90a9b575b7aee481378596c17885588865ece912c7b7d073e68a2948dbbac0bf"
-                )
+            name: "DyteiOSCore",
+            url: "https://github.com/dyte-in/DyteMobileCoreiOS/archive/refs/tags/0.4.7.zip",
+            checksum: "2d0b4777db3edd2036874e74b2e55e33b4adb73a2087c9609cc216124dd9892e"
+        ),
+        .target(
+            name: "DyteiOSCoreTargets",
+            dependencies: [
+                .target(name: "DyteiOSCore"),
+                .product(name: "WebRTC", package: "WebRTC"),
+                .product(name: "DyteiOSSocketIO", package: "DyteiOSSocketIO"),
+            ],
+            path: "Sources/Wrapper"
+        )
     ]
 )
