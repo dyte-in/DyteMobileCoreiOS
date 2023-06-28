fce1b5f50cc402c01139a35544e06cd046ef83e0
Shaunak Jagtap <shaunakjagtap@gmail.com>
2023-06-28 16:39:50 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DyteiOSCore",
-            url: "https://github.com/dyte-in/DyteMobileCoreiOS/archive/refs/tags/0.5.1.zip",
+            url: "https://github.com/dyte-in/DyteMobileCoreiOS/archive/refs/tags/0.5.2.zip",
             checksum: "2ddf6040ff832ed0c2b43d8971a15adb8c904b777557a581a29cd8da50ddc3c6"
         ),
         .target(
