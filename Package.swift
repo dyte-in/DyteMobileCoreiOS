fd6234e3db823d6f612fce891462d2e8cc61ce6a
Shaunak Jagtap <shaunakjagtap@gmail.com>
2023-06-28 17:37:46 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "DyteiOSCore", targets: ["DyteiOSCoreTargets"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/dyte-in/DyteiOSSocketIO.git", revision: "0.0.6"),
+        .package(url: "https://github.com/dyte-in/DyteiOSSocketIO.git", revision: "0.1.0"),
         .package(name: "WebRTC", url: "https://github.com/jitsi/webrtc.git", branch: "master"),
     ],
     targets: [
