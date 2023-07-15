2c08b053e806cbb1cfb3c8262b687bbef1d40a5c
Shaunak Jagtap <shaunakjagtap@gmail.com>
2023-07-15 21:05:15 +0530

    fix: spm stability

--- Package.swift
+++ Package.swift
@@ -5,11 +5,7 @@ let package = Package(
     name: "DyteMobileCoreiOS",
     platforms: [.iOS(.v13)],
     products: [
-        .library(name: "DyteiOSCore", targets: ["DyteiOSCoreTargets"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/dyte-in/DyteiOSSocketIO.git", revision: "0.1.4"),
-        
+        .library(name: "DyteiOSCore", targets: ["DyteiOSCore", "WebRTC", "DyteiOSSocketIO", "Starscream"]),
     ],
     targets: [
         .binaryTarget(
@@ -19,17 +15,18 @@ let package = Package(
         ),
         .binaryTarget(
             name: "DyteiOSCore",
-                        url: "https://github.com/dyte-in/DyteMobileCoreiOS/archive/refs/tags/0.6.0.zip",
-                        checksum: "7b3b683d2dd05a3ec6be86331d8387aa421c5eaa330bb929cb5a6142ddaa7cca"
+            url: "https://github.com/dyte-in/DyteMobileCoreiOS/archive/refs/tags/0.6.0.zip",
+            checksum: "7b3b683d2dd05a3ec6be86331d8387aa421c5eaa330bb929cb5a6142ddaa7cca"
         ),
-        .target(
-            name: "DyteiOSCoreTargets",
-            dependencies: [
-                .target(name: "DyteiOSCore"),
-                .target(name: "WebRTC"),
-                .product(name: "DyteiOSSocketIO", package: "DyteiOSSocketIO"),
-            ],
-            path: "Sources/Wrapper"
+        .binaryTarget(
+            name: "DyteiOSSocketIO",
+            url: "https://github.com/dyte-in/DyteiOSSocketIO/archive/refs/tags/0.1.3.zip",
+            checksum: "ca0ffe1930f5d5f472abb4966b2b814489b463399fb7aa757b2ab8bd3570048e"
+        ),
+        .binaryTarget(
+            name: "Starscream",
+            url: "https://github.com/dyte-in/DyteStarscream/archive/refs/tags/0.0.1.zip",
+            checksum: "8aa8a8078b74aa4dcb86bf27ce2b46ef51b5e5f731226cd30fbd83ac5d1e633f"
         )
     ]
 )
