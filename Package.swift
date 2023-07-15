424a8e666efcf73dbaed4007a1506021768d59e2
Shaunak Jagtap <shaunakjagtap@gmail.com>
2023-07-15 20:07:36 +0530

    release: SPM stable release

--- Package.swift
+++ Package.swift
@@ -2,26 +2,31 @@
 import PackageDescription
 
 let package = Package(
-    name: "DyteiOSCore",
+    name: "DyteMobileCoreiOS",
     platforms: [.iOS(.v13)],
     products: [
         .library(name: "DyteiOSCore", targets: ["DyteiOSCoreTargets"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/dyte-in/DyteiOSSocketIO.git", revision: "0.1.3"),
-        .package(name: "WebRTC", url: "https://github.com/jitsi/webrtc.git", branch: "master")
+        .package(url: "https://github.com/dyte-in/DyteiOSSocketIO.git", revision: "0.1.4"),
+        
     ],
     targets: [
         .binaryTarget(
+            name: "WebRTC",
+            url: "https://github.com/jitsi/webrtc/releases/download/v111.0.2/WebRTC.xcframework.zip",
+            checksum: "5033f23040628e76baa3a9c83c28d89e86ce8127a5a83b5b7d077ede24182b07"
+        ),
+        .binaryTarget(
             name: "DyteiOSCore",
-            url: "https://github.com/dyte-in/DyteMobileCoreiOS/archive/refs/tags/0.6.0.zip",
-            checksum: "98ae7326fb51c875c9671969967527f0574a031f4a7de39606c83e6f758ccfd9"
+                        url: "https://github.com/dyte-in/DyteMobileCoreiOS/archive/refs/tags/0.6.0.zip",
+                        checksum: "7b3b683d2dd05a3ec6be86331d8387aa421c5eaa330bb929cb5a6142ddaa7cca"
         ),
         .target(
             name: "DyteiOSCoreTargets",
             dependencies: [
                 .target(name: "DyteiOSCore"),
-                .product(name: "WebRTC", package: "WebRTC"),
+                .target(name: "WebRTC"),
                 .product(name: "DyteiOSSocketIO", package: "DyteiOSSocketIO"),
             ],
             path: "Sources/Wrapper"
