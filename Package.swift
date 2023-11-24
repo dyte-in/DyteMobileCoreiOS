a29ef7a25b50b6d1cf321ad2498760da8c0b0e98
Shaunak Jagtap <shaunakjagtap@gmail.com>
2023-11-24 16:42:51 +0530

    release: 1.25.2

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
-let remoteKotlinUrl = "https://github.com/dyte-in/DyteMobileCoreiOS/archive/refs/tags/1.25.0.zip"
+let remoteKotlinUrl = "https://github.com/dyte-in/DyteMobileCoreiOS/archive/refs/tags/1.25.2.zip"
 let remoteKotlinChecksum = "e5de9561f20c33436b64c3db783a432e1ebdaae928db12325a5f00788ca958c0"
 let packageName = "DyteiOSCore"
 // END KMMBRIDGE BLOCK
