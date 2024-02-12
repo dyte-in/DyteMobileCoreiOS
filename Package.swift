86690c47d3def18af568960bb52fdf2c47a4adee
dyte-devel <76557270+dyte-devel@users.noreply.github.com>
2024-02-12 11:26:21 +0000

    chore(release): update SPM definition

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 import PackageDescription
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
-let remoteKotlinUrl = "https://dyte-assets.s3.ap-south-1.amazonaws.com/sdk/ios_core/DyteiOSCore-1.31.5-154b0118-aa07-4bf7-8510-cfe9251fd216.xcframework.zip"
-let remoteKotlinChecksum = "e96ac031f6f271aec01caed8174a90524ed1a39fedbb21fda03b88d6e72343dc"
+let remoteKotlinUrl = "https://dyte-assets.s3.ap-south-1.amazonaws.com/sdk/ios_core/DyteiOSCore-1.31.6-4627a823-529b-4fd9-a2b9-6eb103829d9e.xcframework.zip"
+let remoteKotlinChecksum = "836a255f84ae0020b923bc7607cbff8f9e60cbfed699ec55f99187ea0477cf15"
 let packageName = "DyteiOSCore"
 // END KMMBRIDGE BLOCK
 
